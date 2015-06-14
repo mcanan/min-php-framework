4729524af5d672f3bbac1338fea50697d758a4f9
Marcos Canán <mcanan@gmail.com>
2015-06-14 07:51:36 -0300

    Cache helper

---
+++ helpers/cache.php
@@ -0,0 +1,32 @@
+<?php
+namespace mcanan\framework\helpers;
+
+function cache_put($filename, $buffer)
+{
+    $filename = "./app/cache/$filename";
+    $tempfilename = $filename.getmypid();
+    if (($fp = fopen($tempfilename, "w")) == false) {
+        return false;
+    }
+    fwrite($fp, $buffer);
+    fclose($fp);
+    rename($tempfilename, $filename);
+    return true;
+}
+
+function cache_get($filename, $expiration = false)
+{
+    $filename = "./app/cache/$filename";
+    // $expiration es en segundos.
+    if ($expiration) {
+        $stat = @stat($filename);
+        if ($stat[9]) {
+            if (time() > $stat[9] + $expiration) {
+                @unlink($filename);
+                return false;
+            }
+        }
+    }
+    return @file_get_contents($cachedir.$filename);
+}
+?>
